783ddad449eea54152e6c90a82bc71db58217bb7
choi-yuna <mjung0226@naver.com>
2024-03-20 03:17:55 +0900

    Initial commit

--- App.js
+++ App.js
@@ -1,94 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-
-//Component
-const Header = (props) => {
-  return <Text>{props.title}</Text>;
-};
-
-const MyProfile = () => {
-  return <Profile
-      name="윤아"
-      url="https://images.mypetlife.co.kr/content/uploads/2023/11/18161317/d6c08aa5-dc1c-46a1-97bb-6782641c1624.jpeg"
-      profileSize={40}
-      />;
-};
-
-const Division = () => {
-  return <Text>Division</Text>;
-};
-
-const FriendSection = () => {
-  return <Text>FriendSection</Text>;
-};
-
-const FriendList = () => {
-  return(
-    <View>
-      <Profile name="민지"url="https://shop.peopet.co.kr/data/goods/388/2022/06/_temp_16557127733930view.jpg"profileSize={30}/>
-      <Profile name="지수"url="https://shop.peopet.co.kr/data/goods/388/2022/06/_temp_16557127733930view.jpg"profileSize={30}/>
-      <Profile name="현서"url="https://shop.peopet.co.kr/data/goods/388/2022/06/_temp_16557127733930view.jpg"profileSize={30}/>
-      <Profile name="지연"url="https://shop.peopet.co.kr/data/goods/388/2022/06/_temp_16557127733930view.jpg"profileSize={30}/>
-      <Profile name="수빈"url="https://shop.peopet.co.kr/data/goods/388/2022/06/_temp_16557127733930view.jpg"profileSize={30}/>
-
-    </View>
-  );
-};
-
-// 함수형 컴포넌트
-// const Profile = (props) => {
-//   return (
-//     <View style={{flexDirection: "row"}}>
-//       <Image 
-//       source={{ 
-//         url: props.url,
-//         }} 
-//       style={{
-//         width:props.profileSize,
-//         height:props.profileSize,
-//       }}
-//       />
-//       <Text>{props.name}</Text>
-//     </View>
-//   );
-// };
-
-
-// 클래스형 컴포넌트
-class Profile extends React.Component {
-  render() {
-    return (
-      <View style={{flexDirection: "row"}}>
-            <Image 
-            source={{ 
-              url: this.props.url,
-              }} 
-            style={{
-              width:this.props.profileSize,
-              height:this.props.profileSize,
-            }}
-            />
-            <Text>{this.props.name}</Text>
-          </View>
-    );
-  }
-}
-
-
-export default function App()  {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Header title = "친구"></Header>
-      <MyProfile></MyProfile>
-      <Division></Division>
-      <FriendSection></FriendSection>
-      <FriendList></FriendList>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -98,4 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
- 
